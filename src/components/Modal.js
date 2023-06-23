63e7e170fb922b0e483dacaf5125ad4cd3cfbc69
pmjuu <jsi04049@gmail.com>
2023-06-24 01:37:00 +0900

    feat: add closing function of modal close modal when user clicks outside the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,16 @@
 import styles from "./Modal.module.css";
 
 export default function Modal({ children, closeModal }) {
+  const handleClosingClick = (e) => {
+    if (e.target === e.currentTarget) {
+      document.body.style.cursor = "default";
+
+      return closeModal();
+    }
+  };
+
   return (
-    <div className={styles.modalOuter}>
+    <div className={styles.modalOuter} onClick={handleClosingClick}>
       <div className={styles.modalBox}>
         <div className={styles.contents}>{children}</div>
         <button className={styles.closeButton} onClick={closeModal}>
